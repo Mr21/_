2ec07ec4a9a7e975d212330b44380bb326b633c8
Mr21 <thomastortorini@gmail.com>
2015-03-30 06:18:13 +0200

    ...

--- mess/spiderdoc/js/header.js
+++ mess/spiderdoc/js/header.js
@@ -1,21 +1,40 @@
-function lg(s) { return console.log(s), true; }
-
 $(function() {
 
 	var
 		jq_header = $("#header"),
 		jq_textSearch = $("form input[type='text']", jq_header),
-		jq_linkSearch = $(".link-search", jq_header)
+		jq_linkSearch = $(".link-search", jq_header),
+		jq_moreBtn = $(".more-btn", jq_header),
+		jq_moreToHide = $(".more-toHide", jq_header),
+		jq_moreToHideContainer = $(".more-toHide-container", jq_header),
+		jq_window = $(window),
+		isShowBtns = false
 	;
 
-	jq_linkSearch.click(function() {
+	jq_linkSearch.click(function(e) {
+		e.preventDefault();
 		jq_header.addClass("show-search");
 		jq_textSearch[0].focus();
-		return false;
 	});
 
 	jq_textSearch.blur(function() {
 		jq_header.removeClass("show-search");
+	});
+
+	jq_moreBtn.click(function() {
+		if (isShowBtns = !isShowBtns) {
+			jq_moreToHide.appendTo(jq_moreToHideContainer);
+			jq_header.addClass("show-btns");
+		} else {
+			jq_moreToHide.insertAfter(jq_moreBtn);
+			jq_header.removeClass("show-btns");
+		}
+		return false;
+	});
+
+	jq_window.click(function(e) {
+		if (isShowBtns && jq_moreToHide.index(e.target) === -1)
+			jq_moreBtn.click();
 	});
 
 });
